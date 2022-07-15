6822c908e99d05a8c1fdf3d8ba63494159603a9f
Justin Welsh <justinwelsh33@gmail.com>
2022-07-15 10:52:36 -0500

    update

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,13 +1,15 @@
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello-world") //#1: Why did we change the urlPattern to "/hello-world"?
+@WebServlet(name = "HelloWorldServlet", urlPatterns = "/hello-world")
 
 public class HelloWorldServlet extends HttpServlet {
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-        response.getWriter().println("<h1>Hello, World!</h1>");
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+//        response.getWriter().println("<h1>Hello, World!</h1>");
+        request.getRequestDispatcher("hello-world.jsp").forward(request, response);
     }
 }
---
+++ src/main/webapp/hello-world.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: justinwelsh
+  Date: 7/15/22
+  Time: 10:02 AM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Hello-World File</title>
+</head>
+<body>
+<h1>Hello World</h1>
+</body>
+</html>
