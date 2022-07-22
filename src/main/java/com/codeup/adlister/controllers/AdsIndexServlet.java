5c04b4e8e5e6e2cbf749a39a7f14c0f49118a5af
Justin Welsh <justinwelsh33@gmail.com>
2022-07-22 13:29:13 -0500

    update

--- src/main/java/com/codeup/adlister/controllers/AdsIndexServlet.java
+++ src/main/java/com/codeup/adlister/controllers/AdsIndexServlet.java
@@ -11,8 +11,12 @@ import java.io.IOException;
 
 @WebServlet(name = "controllers.AdsIndexServlet", urlPatterns = "/ads")
 public class AdsIndexServlet extends HttpServlet {
+
+//    When user initially visits the page.
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//        Grabbing data from database.
         request.setAttribute("ads", DaoFactory.getAdsDao().all());
+//        Forwarding request obj to inturpret jsp file
         request.getRequestDispatcher("/WEB-INF/ads/index.jsp").forward(request, response);
     }
 }
